5cba0d5f1ca9476263bd4a3255c6da42a3929f58
Varun Krishna S <varunhawk19@gmail.com>
2023-03-05 04:24:13 +0530

    feat: adds smtp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union, Any
 from fastapi import FastAPI, HTTPException, status
 import uvicorn
 import os
@@ -7,16 +7,54 @@ from pydantic import BaseModel
 from email.parser import Parser
 from email.header import decode_header
 from email.utils import parseaddr
+import smtplib, ssl
+import mailparser
+from fastapi.middleware.cors import CORSMiddleware
 
+# from envelope import Envelope
+import email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 port = int(os.environ["PORT"])
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class User(BaseModel):
     username: str
     password: str
+
+
+class Message(BaseModel):
+    from_addr: str
+    password: str
+    subject: str
+    body: str
+    to_addr: str
+
+
+class BaseResponse(BaseModel):
+    data: str
+
+
+class FetchMailResponse_(BaseModel):
+    number_of_messages: int
+    total_size: int
+    messages: List[Any]
+
+
+class FetchMailResponse(BaseModel):
+    data: FetchMailResponse_
 
 
 def pop_login(user: User):
@@ -33,17 +71,17 @@ def pop_login(user: User):
 
 def clean_msg(lines: List[bytes]):
     msg_content = b"\r\n".join(lines).decode("utf-8")
-    msg = Parser().parsestr(msg_content)
-    return msg
+    mail = mailparser.parse_from_string(msg_content)
+    return mail
 
 
-@app.get("/")
-async def main():
-    return {"hello": "world"}
+@app.get("/ping")
+async def main() -> BaseResponse:
+    return BaseResponse(**({"data": "pong"}))
 
 
-@app.get("/api/login")
-async def login(user: User):
+@app.post("/api/login/")
+async def login(user: User, response_model: BaseResponse):
     pop_client_instance = pop_login(user)
     match pop_client_instance:
         case None:
@@ -53,11 +91,11 @@ async def login(user: User):
             )
         case pop_client_instance:
             pop_client_instance.quit()
-            return {"data": "Successfully Authenticated"}
+            return BaseResponse(**({"data": "Successfully Authenticated"}))
 
 
-@app.get("/api/messages")
-async def messages(user: User):
+@app.post("/api/messages/")
+async def messages(user: User, response_model: dict[str, FetchMailResponse]):
     pop_client_instance = pop_login(user)
     match pop_client_instance:
         case None:
@@ -67,34 +105,52 @@ async def messages(user: User):
             )
         case pop_client_instance:
             (numMsgs, totalSize) = pop_client_instance.stat()
-            messages = []
+            messages: List[mailparser.MailParser] = []
 
             for i in range(1, numMsgs + 1):
                 (header, lines, octets) = pop_client_instance.retr(i)
                 print(
-                    {
-                        "id": i,
-                        "header": header,
-                        "msg": clean_msg(lines),
-                        "octets": octets,
-                    }
+                    clean_msg(lines),
                 )
                 messages.append(
-                    {
-                        "id": i,
-                        "header": header,
-                        "msg": clean_msg(lines),
-                        "octets": octets,
-                    }
+                    clean_msg(lines),
                 )
             pop_client_instance.quit()
             return {
-                "data": {
-                    "numMsgs": numMsgs,
-                    "totalSize": totalSize,
-                    "messages": messages,
-                }
+                "data": FetchMailResponse(
+                    **{
+                        "number_of_messages": numMsgs,
+                        "total_size": totalSize,
+                        "messages": messages,
+                    }
+                )
             }
+
+
+@app.post("/api/message/")
+async def message(message: Message, response_model: BaseResponse):
+    try:
+        message_mime = MIMEMultipart()
+        message_mime["From"] = message.from_addr
+        message_mime["To"] = message.to_addr
+        message_mime["Subject"] = message.subject
+        # message_mime["Bcc"] = receiver_email  # Recommended for mass emails
+
+        # Add body to email
+        message_mime.attach(MIMEText(message.body, "plain"))
+        text = message_mime.as_string()
+        # Log in to server using secure context and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("box.0xparc.space", 465, context=context) as server:
+            server.login(message.from_addr, message.password)
+            server.sendmail(message.from_addr, message.to_addr, text)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Mail transfer failed",
+        )
+    else:
+        return BaseResponse(**{"data": "Mail transfer succeded"})
 
 
 uvicorn.run(app, host="0.0.0.0", port=port)
